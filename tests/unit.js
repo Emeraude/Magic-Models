5c82bb666671e4117bc25dbe67e2bdf18aa245ba
Thibaut Broggi <broggi_t@epitech.eu>
2015-04-09 05:17:10 +0200

    adding unit tests for validation rules at creation

--- tests/unit.js
+++ tests/unit.js
@@ -34,6 +34,19 @@ exports.query = {
 	    test.equal(r.length, i.numRows);
 	    test.done();
 	});
+    },
+
+    cleaningTables: function(test) {
+	db.query('TRUNCATE TABLE Users', function(e, r, i) {
+	    test.equal(e, undefined, e);
+	    db.query('TRUNCATE TABLE News', function(e, r, i) {
+		test.equal(e, undefined, e);
+		db.query('TRUNCATE TABLE Messages', function(e, r, i) {
+		    test.equal(e, undefined, e);
+		    test.done();
+		});
+	    });
+	});
     }
 }
 
@@ -81,7 +94,7 @@ exports.defineModel = {
 	},
 
 	user: function(test) {
-	    test.deepEqual(db.models.User.fields, {id: {fieldName: 'id', type: 'int', key: 'primary', default: {created: null, modified: null}}, login: {fieldName: 'login', type: 'varchar', length: 32, key: 'unique', validate: {len: [4, 32], is: /^[a-z0-9\-_\.]{4,32}$/i, notIn: ['root', 'admin'], isUnique: true}, default: { created: 'anonymous', modified: null}}, password: {fieldName: 'password', type: 'varchar', length: 255, validate: {validPassword: {minLen: 8, required: true, msg: 'Password not strong enough'}}, default: {created: null, modified: null}}, mail: {fieldName: 'mail', type: 'varchar', length: 255, validate: {maxLen: 255}, default: {created: null, modified: null}}}, 'Model `User` not defined correctly');
+	    test.deepEqual(db.models.User.fields, {id: {fieldName: 'id', type: 'int', key: 'primary', default: {created: null, modified: null}}, login: {fieldName: 'login', type: 'varchar', length: 32, key: 'unique', validate: {len: [4, 32], is: /^[a-z0-9\-_\.]{4,32}$/i, notIn: ['root', 'admin'], required: true, isUnique: true}, default: { created: 'anonymous', modified: null}}, password: {fieldName: 'password', type: 'varchar', length: 255, validate: {validPassword: {minLen: 8, required: true, msg: 'Password not strong enough'}}, default: {created: null, modified: null}}, mail: {fieldName: 'mail', type: 'varchar', length: 255, validate: {maxLen: 255}, default: {created: null, modified: null}}}, 'Model `User` not defined correctly');
 	    test.equal(db.models.User.table, 'Users', 'Wrong table name for model `User`');
 	    test.equal(db.models.User.primaryKey, 'id', 'Wrong primary key for model `User`');
 	    test.equal(db.models.User.createdAt, 'createdAt', 'Wrong createdAt field for model `User`');
@@ -105,6 +118,73 @@ exports.defineModel = {
 	    test.equal(db.models.Message.createdAt, 'createdAt', 'Wrong createdAt field for model `Message`');
 	    test.equal(db.models.Message.modifiedAt, null, 'Wrong modifiedAt field for model `Message`');
 	    test.done();
+	}
+    }
+}
+
+// TODO : validation rules
+
+exports.modelFunctions = {
+    create: {
+	badTests: {
+	    EmptyObject: function(test) {
+		db.models.User.create({}, function(e, r, i) {
+		    test.equal(i, undefined, 'This test should fail');
+		    test.deepEqual(e, {validationErrors: ['Rule "required" for field `login` has not been successfully validated.', 'Password not strong enough']}, 'This test should fail');
+		    test.done();
+		});
+	    },
+
+	    invalidValidations: {
+		is: function(test) {
+		    db.models.User.create({login: ''}, function(e, r, i) {
+			test.equal(i, undefined, 'This test should fail');
+			test.deepEqual(e, {validationErrors: ['Rule "len" for field `login` has not been successfully validated.', 'Rule "is" for field `login` has not been successfully validated.', 'Password not strong enough']}, 'This test should fail');
+			test.done();
+		    });
+		},
+
+		len: function(test) {
+		    db.models.User.create({login: 'foobarfoobarfoobarfoobarfoobarfoobar'}, function(e, r, i) {
+			test.equal(i, undefined, 'This test should fail');
+			test.deepEqual(e, {validationErrors: ['Rule "len" for field `login` has not been successfully validated.', 'Rule "is" for field `login` has not been successfully validated.', 'Password not strong enough']}, 'This test should fail');
+			test.done();
+		    });
+		},
+
+		notIn: function(test) {
+		    db.models.User.create({login: 'root'}, function(e, r, i) {
+			test.equal(i, undefined, 'This test should fail');
+			test.deepEqual(e, {validationErrors: ['Rule "notIn" for field `login` has not been successfully validated.', 'Password not strong enough']}, 'This test should fail');
+			test.done();
+		    });
+		},
+
+		required: function(test) {
+		    db.models.User.create({login: undefined},  function(e, r, i) {
+			test.equal(i, undefined, 'This test should fail');
+			test.deepEqual(e, {validationErrors: ['Rule "required" for field `login` has not been successfully validated.', 'Password not strong enough']}, 'This test should fail');
+			test.done();
+		    });
+		},
+
+		minLen: function(test) {
+		    db.models.User.create({login: undefined, password: 'foobar2'},  function(e, r, i) {
+			test.equal(i, undefined, 'This test should fail');
+			test.deepEqual(e, {validationErrors: ['Rule "required" for field `login` has not been successfully validated.', 'Password not strong enough']}, 'This test should fail');
+			test.done();
+		    });
+		},
+
+		maxLen: function(test) {
+		    db.models.User.create({login: undefined, password: 'foobar2', mail: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'},  function(e, r, i) {
+			test.equal(i, undefined, 'This test should fail');
+			test.deepEqual(e, {validationErrors: ['Rule "required" for field `login` has not been successfully validated.', 'Password not strong enough', 'Rule "maxLen" for field `mail` has not been successfully validated.']}, 'This test should fail');
+			test.done();
+		    });
+
+		},
+	    }
 	}
     }
 }
