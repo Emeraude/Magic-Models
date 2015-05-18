55bd90d8c035588e0a2601aee9cb667243327642
Thibaut Broggi <broggi_t@epitech.eu>
2015-05-18 10:54:58 +0200

    model.find() also works without object

--- tests/find.js
+++ tests/find.js
@@ -1,4 +1,13 @@
 exports.find = {
+    noObject: function(test) {
+	db.models.User.find(function(e, r, i) {
+	    test.equal(e, undefined, 'An error occured');
+	    test.deepEqual(r, {id: '1', login: 'foobar', password: 'foobar42', mail: null}, 'Invalid results object');
+	    test.deepEqual(i, {insertId: 1, affectedRows: 0, numRows: 1, query: 'SELECT `id` AS `id`, `login` AS `login`, `password` AS `password`, `mail` AS `mail` FROM `Users` LIMIT 1'}, 'Invalid informations object');
+	    test.done();
+	});
+    },
+
     emptyObject: function(test) {
 	db.models.User.find({}, function(e, r, i) {
 	    test.equal(e, undefined, 'An error occured');
