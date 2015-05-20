d0edf0480cf8ba1207fee2a37d18b88264ac7aeb
Thibaut Broggi <broggi_t@epitech.eu>
2015-05-20 05:40:56 +0200

    fix updates for empty object (with unit test)

--- tests/update.js
+++ tests/update.js
@@ -5,5 +5,13 @@ exports.update = {
 	    test.deepEqual(i, {insertId: 0, affectedRows: 0, numRows: 0, query: 'UPDATE `Users` SET `modifiedAt` = NOW()'}, 'Invalid results object');
 	    test.done();
 	});
+    },
+
+    emptyObject: function(test) {
+	db.models.User.update({}, function(e, r, i) {
+	    test.equal(e, undefined, 'An error occured');
+	    test.deepEqual(i, {insertId: 0, affectedRows: 0, numRows: 0, query: 'UPDATE `Users` SET `modifiedAt` = NOW()'}, 'Invalid results object');
+	    test.done();
+	});
     }
 }
