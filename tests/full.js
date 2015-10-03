22cb6caacdc04d40fee68f127d99213f7fb9c8b4
Thibaut Broggi <broggi_t@epitech.eu>
2015-10-03 12:59:56 +0200

    adding RegExp management for escape()

--- tests/full.js
+++ tests/full.js
@@ -13,6 +13,7 @@ exports.escape = function(test) {
   test.equal('true', escape(true), 'Fail test to escape bool');
   test.equal('"\\\\foo\\nbar\\r\\\'\\\\"', escape("\\foo\nbar\r'\\"), 'Fail test to escape string');
   test.equal('2015-08-13 01:17:44', escape(new Date('Thu Aug 13 2015 01:17:44')), 'Fail test to escape date');
+  test.equal('[\\w.]*@\\w*.\\w{2,}', escape(/[\w.]*@\w*.\w{2,}/i), 'Fail test to escape regexp');
   test.equal('NULL', escape(null));
   test.done();
 }
