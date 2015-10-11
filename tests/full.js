f0f3765138a86ab47f5d21dfeda0bd57c6d10f25
Thibaut Broggi <broggi_t@epitech.eu>
2015-10-11 17:54:39 +0200

    add randomCase management in ORDER BY clause

--- tests/full.js
+++ tests/full.js
@@ -93,12 +93,21 @@ exports.queryBuilder = {
     test.done();
   },
 
-  order: function(test) {
-    test.equal(' ORDER BY `login` ASC', queryBuilder({order: {login: 'asc'}}));
-    test.equal(' ORDER BY `login` ASC, `mail` DESC', queryBuilder({order: {login: 'asc', mail: 'desc'}}));
-    test.equal(' ORDER BY `userName` ASC', queryBuilder({order: {login: 'asc'}}, aliases));
-    test.equal(' ORDER BY `userName` ASC, `email` DESC', queryBuilder({order: {login: 'asc', mail: 'desc'}}, aliases));
-    test.done();
+  order: {
+    standard: function(test) {
+      test.equal(' ORDER BY `login` ASC', queryBuilder({order: {login: 'asc'}}));
+      test.equal(' ORDER BY `login` ASC, `mail` DESC', queryBuilder({order: {login: 'asc', mail: 'desc'}}));
+      test.equal(' ORDER BY `userName` ASC', queryBuilder({order: {login: 'asc'}}, aliases));
+      test.equal(' ORDER BY `userName` ASC, `email` DESC', queryBuilder({order: {login: 'asc', mail: 'desc'}}, aliases));
+      test.done();
+    },
+    randomCase: function(test) {
+      test.equal(' ORDER BY `login` ASC', queryBuilder({order: {login: 'ASc'}}));
+      test.equal(' ORDER BY `login` ASC, `mail` DESC', queryBuilder({order: {login: 'aSC', mail: 'Desc'}}));
+      test.equal(' ORDER BY `userName` ASC', queryBuilder({order: {login: 'aSc'}}, aliases));
+      test.equal(' ORDER BY `userName` ASC, `email` DESC', queryBuilder({order: {login: 'asC', mail: 'DESc'}}, aliases));
+      test.done();
+    }
   },
 
   limit: function(test) {
